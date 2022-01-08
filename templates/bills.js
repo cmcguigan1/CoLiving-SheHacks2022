9f3646c501c1322627768832fa699cfd40898c3e
tarazoelee <77064834+tarazoelee@users.noreply.github.com>
2022-01-08 13:52:24 -0500

    fixed my js

--- templates/bills.js
+++ templates/bills.js
@@ -1,9 +1,7 @@
-function getInputValue(){
-  var number = document.getElementById("number");
-  var text = document.getElementById("text");
-  var date = document.getElementById("date");
- 
-  var e = document.getElementById("dropdownlist");
-  var person = e.options[e.selectedIndex].text;
-  
+//onclick function 
+document.getElementById("button").onclick = function(){
+
+  var amount = document.getElementById("number").value; //getting the inputted value 
+  //var total = amount + document.getElementById("name1").innerText;
+  document.getElementById("name1").innerText = amount;
 }
