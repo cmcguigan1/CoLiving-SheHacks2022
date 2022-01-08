3b739c8cea5849bbcae18c44bccf5555c1018f74
tarazoelee <77064834+tarazoelee@users.noreply.github.com>
2022-01-08 14:05:43 -0500

    woohoo

--- templates/bills.js
+++ templates/bills.js
@@ -1,7 +1,28 @@
 //onclick function 
 document.getElementById("button").onclick = function(){
+  var e = document.getElementById("dropdownlist");
+  var person = e.options[e.selectedIndex].text;
 
+  if (person ==="Tara"){
   var amount = document.getElementById("number").value; //getting the inputted value 
   //var total = amount + document.getElementById("name1").innerText;
-  document.getElementById("name1").innerText = amount;
+  document.getElementById("name1").innerText = "$" + amount;
+  }
+
+  if (person ==="Claire"){
+    var amount = document.getElementById("number").value; //getting the inputted value 
+    //var total = amount + document.getElementById("name1").innerText;
+    document.getElementById("name2").innerText = "$" + amount;
+    }
+
+    if (person ==="Kaitlyn"){
+      var amount = document.getElementById("number").value; //getting the inputted value 
+      //var total = amount + document.getElementById("name1").innerText;
+      document.getElementById("name3").innerText = "$" + amount;
+      }
+      if (person ==="Lauren"){
+        var amount = document.getElementById("number").value; //getting the inputted value 
+        //var total = amount + document.getElementById("name1").innerText;
+        document.getElementById("name4").innerText = "$" + amount;
+        }
 }
