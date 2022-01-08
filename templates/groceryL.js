dde181a691d9c3183028200586d72979cdd4145b
laurenwhitmore <90729743+laurenwhitmore@users.noreply.github.com>
2022-01-07 21:49:18 -0700

    heyhey

--- templates/groceryL.js
+++ templates/groceryL.js
@@ -1,21 +1,65 @@
-var list = [];
-var number= window.prompt("How many items would you like to add");
-for(var i=0; i<number;i++){
-    var item= window.prompt("Enter your grocery request: ");
-add(item);
-}
-displayList(list)
-function add(item){
-    list.push(item)
-}
-function remove(item){
-    list.remove(item);
-}
-function displayList(list){
-    for(var i=0; i<list.length; i++){
-        if(list==0){
-            return
+const groceries=[];
+
+class Grocieries{
+    constructor(item){
+        this.ulElement=item;
+    }
+    add(){
+        const inputs= document.querySelector("#myInput").value;
+        if(inputs==""){
+        alert("You need to eneter an grocery!!")}
+        else{
+            const groceryObject={
+                id: groceries.length,
+                itemNAme: inputs,
+                isDone:false,
+            }
+            groceries.unshift(groceryObject);
+            this.display();
+            document.querySelector("#myInput").value='';
         }
-        alert(list[i]);
+    }
+    found(x){
+        const itemIndex=groceries.findIndex((item)=>item.id==x);
+        console.log(groceries[itemIndex].isDone);
+        groceries[itemIndex].isDone==false? groceries[itemIndex].isDone=true:groceries[itemIndex].isDone=false;
+        this.display();
+    }
+    deleteFood(y){
+        const intemIndex=groceries.findIndex((item)=>item.id==y);
+        groceries.splice(itemIndex,1);
+        this.display();
+    }
+    display(){
+        this.ulElement.innerHTML="";
+        groceries.forEach((item)=>{
+            const liElement=document.createElement("li");
+            const delBTn=document.createElement("del");
+            liElement.innerText=item.itemNAme;
+            liElement.setAttribute("data-id",item.id);
+
+            delBTn.setAttribute("data-id",item.id)
+            delBTn.classList.add("far","fa-trash-alt");
+            liElement.appendChild(delBTn);
+            delBTn.addEventListener("click",function(e){
+                const deleteId=e.target.getAttribute("data-id");
+                myList.deleteElement(deleteId);
+            })
+            liElement.addEventListener("click",function(e){
+                const selectedId=e.target.getAttribute("data-id");
+                myList.found(selectedId);
+            })
+            if(item.isDone){
+                liElement.classList.add("checked");
+            }
+            this.ulElement.appendChild(liElement);
+        })
+
     }
 }
+
+const listSelection=document.querySelector("#myUL");
+myList=new Grocieries(listSelection);
+document.querySelector(".addBTn").addEventListener("click",function(){
+    myList.add()
+})
