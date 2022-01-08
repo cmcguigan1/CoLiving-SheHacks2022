85ee67ce971ea120dd44fed39541d58fde585201
tarazoelee <77064834+tarazoelee@users.noreply.github.com>
2022-01-08 18:08:59 -0500

    FIXEDIT

--- templates/bills.js
+++ templates/bills.js
@@ -1,35 +1,66 @@
 //onclick function 
+
 document.getElementById("button").onclick = function(){
+
+
   var e = document.getElementById("dropdownlist");
   var person = e.options[e.selectedIndex].text;
 
+
   if (person ==="Tara"){
-  var amount = document.getElementById("number").value; //getting the inputted value 
-  //var total = amount + document.getElementById("name1").innerText;
-  document.getElementById("name1").innerText = "$" + amount;
+    var amount = document.getElementById("number").value;
+    if(document.getElementById("name1").innerText>0){
+      previous = document.getElementById("name1").innerText
+      document.getElementById("name1").innerText = Number(amount) + Number(previous);
+    }
+    else{
+     //getting the inputted value 
+      document.getElementById("name1").innerText = amount;
+    }
   }
 
   if (person ==="Claire"){
-    var amount = document.getElementById("number").value; //getting the inputted value 
-    //var total = amount + document.getElementById("name1").innerText;
-    document.getElementById("name2").innerText = "$" + amount;
+    var amount = document.getElementById("number").value;
+    if(document.getElementById("name2").innerText>0){
+      previous = document.getElementById("name2").innerText
+      document.getElementById("name2").innerText = Number(amount) + Number(previous);
+    }
+    else{
+     //getting the inputted value 
+      document.getElementById("name2").innerText = amount;
+    }
     }
 
   if (person ==="Kaitlyn"){
-      var amount = document.getElementById("number").value; //getting the inputted value 
-      //var total = amount + document.getElementById("name1").innerText;
-      document.getElementById("name3").innerText = "$" + amount;
+    var amount = document.getElementById("number").value;
+    if(document.getElementById("name3").innerText>0){
+      previous = document.getElementById("name3").innerText
+      document.getElementById("name3").innerText = Number(amount) + Number(previous);
+    }
+    else{
+     //getting the inputted value 
+      document.getElementById("name3").innerText = amount;
+    }
       }
+
   if (person ==="Lauren"){
-      var amount = document.getElementById("number").value; //getting the inputted value 
-        //var total = amount + document.getElementById("name1").innerText;
-      document.getElementById("name4").innerText = "$" + amount;
+    var amount = document.getElementById("number").value;
+    if(document.getElementById("name4").innerText>0){
+      previous = document.getElementById("name1").innerText
+      document.getElementById("name4").innerText = Number(amount) + Number(previous);
+    }
+    else{
+     //getting the inputted value 
+      document.getElementById("name4").innerText = amount;
+    }
       }
+
       publishhistory();
 
       let inputs = document.querySelectorAll("input");
       inputs.forEach((input) => (input.value = ""));
 };
+
 
 function publishhistory(){
   
